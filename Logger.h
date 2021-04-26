a0df2115973c46a34b47e3edc3e2c3b582244edc
Kiss Attila <nmsstulaj@gmail.com>
2021-04-26 09:15:46 +0200

    reduce log buffer size to 64 bytes from 256

--- Logger.h
+++ Logger.h
@@ -98,7 +98,7 @@ private:
 #ifdef _DEBUG /* this is only for debugging, it remains oldschool */
 #define DBG(str, ...) \
     {\
-        char __debug_format_str[256]; \
+        char __debug_format_str[64]; \
         snprintf(__debug_format_str, sizeof(__debug_format_str), str, __VA_ARGS__); \
         OutputDebugStringA(__debug_format_str); \
     }
